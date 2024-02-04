4d88d2eef855dc90f8a9a56fcfe0b32aebe0267f
Suayb <byausrimed@gmail.com>
2024-02-04 22:42:44 +0300

    commit

--- DataStructures/Non-linearDataStructures/Graph.cpp
+++ DataStructures/Non-linearDataStructures/Graph.cpp
@@ -4,11 +4,101 @@ using namespace std;
 
 class Node{
 	public:
-		int data;
+		int vertex;
+		int weight
 		Node *left,*right;
 };
 
+void create(Node *head)
+{
+	char ch='y';
+	int v1,v2,choice,no,weight;
+	Node*newNode;
+	Node*temp;
+	cout<<"0-Directed Graph\n";
+	cout<<"1-Undirected Graph\n";
+	cout<<"Enter your choice (0 or 1):\n";
+	
+	for(size_t i = 0 ; i<no ; i++)
+	{
+		cout<<"\n Enter the starting node ending node and weight:\n";
+		
+		cin>>v1;
+		cin>>v2;
+		cin>>weight;
+	}
+	
+	newNode = new Node();
+	newNode->vertex = v2;
+	newNode->weight=weight;
+	
+	temp = head[v1];
+	if(temp==NULL)
+	{
+		head[v1] = newNode;
+	}
+	else
+	{
+		while(temp->next!=NULL)
+		{
+			temp=temp->next;
+		}
+		temp->next = newNode;
+	}
+	
+	if(choice==1)
+	{
+		newNode = new Node();
+		newNode->vertex = v1;
+		newNode->weight = weight;
+		temp = head[v2];
+		if(temp==NULL)
+		{
+			head[v2] = newNode;
+		}
+		else
+		{
+			while(temp->next!=NULL)
+			{
+				temp = temp->next;
+			}
+			temp->next = newNode;
+		}
+	}
+}
+
+void display(Node *head[],int n)
+{
+	int v;
+	Node *adj;
+	cout<<"Adjancency List Is:\n";
+	for(v = 0; v<n ; v++)
+	{
+		cout<<"Head["<<v<<"]";
+		adj = head[v];
+		while(adj!=NULL)
+		{
+			cout<<adj->vertex<<"=>weight:"<<adj->weight<<" ";
+			adj = adj->next;
+		}
+		
+		cout<<"\n";
+	}
+}
+
 int main(void)
 {
+	char c = "y";
+	int ch,start,n,visited[10],v;
+	Node *head[50];
+	cout<<"No. of vertices in the graph:\n";
+	cin>>n;
+	for(v = 0; v<n ; v++)
+	{
+		head[v] = NULL;
+	}
+	
+	create(head);
+	display(haed,n);
 	return 0;
 }
