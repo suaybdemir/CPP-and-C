26cea2c464582b289fa78ae3252d21966a1d0cce
Suayb <byausrimed@gmail.com>
2024-02-08 16:47:20 +0300

    Patch

--- DataStructures/LinearDataStructures/Vectors/example2.cpp
+++ DataStructures/LinearDataStructures/Vectors/example2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 template<class T>
@@ -6,26 +7,50 @@ class Vector{
 	T*data;
 	int cap;
 	int index;
-	public:
-		Vector() : data(NULL),index(-1),cap(1){
-			data = new T[cap];
+		void growth(){
+			if(size()>capacity()){
+				cap *= 2;
+				T *tmp = new T[cap];
+				for(int i=0 ; i<size() ; i++)
+					tmp[i] = data[i];
+				free(data);
+				data = tmp;
+			}
 		}
-		
+		void shrink(){
+			if(size()<capacity()/2 && capacity()!=1 )
+			{
+				cap /= 2;
+				T *tmp = new T[cap];
+				for(int i=0; i<size(); i++)
+				{
+					tmp[i] = data[i];
+				}
+				free(data);
+				data = tmp; 
+			}
+		}
+	public:
+		Vector() : data(NULL),index(-1),cap(1){ data = new T[cap]; }
+	
 		int size()const{ return index; }
-		int capacity()const{ return cap; }
+		int capacity()const{ 
+			return cap;
+		}
 		void push_back(const T& value)
 		{
+			growth();
 			data[++index]=value;
 		}
-		void pop_back()
-		{
-			index--;
+		void pop_back(){ 
+			if(isEmpty())
+				throw "Error : Vector is empty.";
+			index--; 
+			shrink();
 		}
 		
-		bool isEmpty()const{
-			return size()==-1;
-		}
-		
+		bool isEmpty()const{return size()==-1;}
+	
 		T front()const{
 			if(isEmpty())
 				throw "Error : Vector is empty.";
@@ -56,6 +81,8 @@ int main(void)
 		v.push_back(20);
 		//v.pop_back();
 		
+		cout<<v.capacity()<<endl;
+		
 		cout<<v.back();
 		
 		cout<<endl;
@@ -63,6 +90,12 @@ int main(void)
 		cout<<v.isEmpty()<<endl;
 		
 		cout<<v.front()<<endl;
+		v.pop_back();
+		cout<<"index : "<< v.size()<<endl;
+		cout<<"capacity : "<< v.capacity()<<endl;
+		v.pop_back();
+		cout<<"index : "<< v.size()<<endl;
+		cout<<"capacity : " << v.capacity()<<endl;
 		
 	}catch(const char* exception)
 	{
