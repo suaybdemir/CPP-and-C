9e91ee46e546f72c007a8b3ddd11862266c25f3f
suaybdemir <byausrimed@gmail.com>
2024-03-19 22:22:22 +0300

    Release Snake Game 1-Fixed Some Bugs 2-Added Tailed Snake

---
+++ SnakeGame/Readme.md
@@ -0,0 +1,10 @@
+# C++ Snake Game
+
+- Version 1.0 -
+	 
+	Tailed Snake
+	Point System
+	Fixed Some Bugs
+	
+---
+	
--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -9,6 +9,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100],tailY[100];
+int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
@@ -41,8 +43,25 @@ void Draw()
             if( i == y && j ==x) cout<< "O";
             
             else if( i == fruitY && j == fruitX ) cout<< "F";
-            else cout<<" ";
-
+            else
+            {
+                bool print = false;
+                for(int k = 0;k<nTail ; k++)
+                {
+                    
+                    if(tailX[k]==j && tailY[k]==i)
+                    {
+                        cout<<"o";
+                        print = true;
+                    }
+                    
+                }
+                if(!print)
+                {
+                    cout<<" ";
+                }
+            }
+            
             if( j== width-1 ) cout<<"*";
         }
         cout<<endl;
@@ -56,45 +75,60 @@ void Draw()
 void Input()
 {
     char key = getch();
-        switch(key)
-        {
-            case 'a':
-                dir=LEFT;
-                break;;
-            case 'd':
-                dir=RIGHT;
-                break;
-            case 'w':
-                dir=UP;
-                break;
-            case 's':
-                dir=DOWN;
-                break;
-            case 'x':
-                gameOver=true;
-                break;
-        }
+    switch(key)
+    {
+        case 'a':
+            dir=LEFT;
+            break;;
+        case 'd':
+            dir=RIGHT;
+            break;
+        case 'w':
+            dir=UP;
+            break;
+        case 's':
+            dir=DOWN;
+            break;
+        case 'x':
+            gameOver=true;
+            break;
     }
+}
 
 void Logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X,prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for(int i=1 ; i<nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
     switch (dir)
     {
-    case LEFT:
-        x--;
-        break;
-    case RIGHT:
-        x++;
-        break;
-    case UP:
-        y--;
-        break;
-    case DOWN:
-        y++;
-        break;
-    default:
-        break;
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
     }
+
     if(x==width)
     {       
         x=width-1;
@@ -116,6 +150,15 @@ void Logic()
         score += 10;
         fruitX = rand()% width;
         fruitY = rand()% height;
+        nTail++;
+    }
+    
+    for(int i=0 ; i<nTail; i++)
+    {
+        if(tailX[i]==x && tailY[i]==y)
+        {
+            gameOver=true;
+        }   
     }
 }
 
