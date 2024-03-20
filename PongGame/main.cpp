aa60632cefd6949eb37351b53649416c034acd8b
suaybdemir <byausrimed@gmail.com>
2024-03-21 01:12:00 +0300

    Edited PongGame 1-Added Paddle class 2-Added GameManager class which contains:
      *Added Up and Down
      *Added Score System
      *Added Default Location determiner

--- PongGame/main.cpp
+++ PongGame/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <time.h>
+#include <ncurses.h>
+#include <cstdlib>
 using namespace std;
 
 enum eDir{STOP=0, LEFT=1, UPLEFT=2, DOWNLEFT=3 , RIGHT=4 , UPRIGHT=5 , DOWNRIGHT=6 };
@@ -68,17 +71,87 @@ class cBall
         }
         
 };
+class cPaddle
+{
+    private:
+        int x,y;
+        int originalX,originalY;
+    public:
+        cPaddle()
+        {
+            x = y = 0;
+        }
+        cPaddle(int posX,int posY) : cPaddle()
+        {
+            originalX = posX;
+            originalY = posY;
+            x = posX;
+            y = posY;
+        }
+        inline void Reset() { x = originalX; y = originalY; }
+        inline int getX() { return x; }
+        inline int getY() { return y; }
+        inline void moveUp() { y--; }
+        inline void moveDown() { y++; }
+        friend ostream & operator<<(ostream & o ,cPaddle  c)
+        {
+            o << "Paddle [" << c.x << "," << c.y << "]";
+            return o;
+        }
+};
+
+class cGameManger
+{
+    private:
+        int width,height;
+        int score1,score2;
+        char up1,down1,up2,down2;
+        bool quit;
+        cBall *ball;
+        cPaddle *player1;
+        cPaddle *player2;
+    public:
+        cGameManger(int w,int h)
+        {
+            srand(time(NULL));
+            quit = false;
+            up1 = 'w' ; up2 = 'i';
+            down1 = 's'; down2 = 'k';
+            score1 = score2 = 0;
+            width = w; height = h;
+            ball = new cBall(w/2,h/2);
+            player1 = new cPaddle(1,h/2 - 3);
+            player2 = new cPaddle(w - 2,h / 2 -3 );
+
+        }
+        ~cGameManger()
+        {
+            delete ball,player1,player2;
+        }
+        void ScoreUp(cPaddle * player)
+        {
+            if(player == player1) score1++;
+            else if(player == player2) score2++;
+
+            ball->Reset();
+            player1->Reset();
+            player2->Reset();
+        }
+};
 
 int main(void)
 {
-    cBall c(0,0);
-    cout<< c <<endl;
-    c.randomDirection();
-    cout<< c <<endl;
-    c.Move();
-    cout<< c <<endl;
-    c.randomDirection();
-    c.Move();
-    cout<< c << endl;
+    Setup();
+    initscr();
+    //cbreak(),endwin();
+    cPaddle p1(0,0);
+    cPaddle p2(10,0);
+    cout<< p1 <<endl;
+    cout<< p2 << endl;
+    p1.moveUp();
+    p2.moveDown();
+    cout<< p1 <<endl;
+    cout<< p2 << endl;
+
     return 0;
 }
